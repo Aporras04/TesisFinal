1fe77c9b737081bca6b7c0680d9b3cbd7af8a760
Andrea Porras <andrea.porras@corporate.endless.net>
2024-01-04 22:55:26 -0500

    Create project

---
+++ PetVet/Controllers/HomeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PetVet.Models;
+
+namespace PetVet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public JsonResult GetMyPets()
+        {
+            var model = new[]
+            {
+                new Mascota
+                {
+                    Nombre= "Candy", Raza= "Mestiza", Edad="1 ano", Especie="perro", Sexo="Hembra", Color="Blanco y Cafe", Esterilizado = true, Veterinaria = "Veterinaria 1"
+                }
+
+            };
+            return Json(model);
+        }
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult SignIn()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult LogIn()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Pets()
+        {
+            return View();
+        }
+
+        public ActionResult NewPet()
+        {
+            return View();
+        }
+
+        public ActionResult PetProfile()
+        {
+            return View();
+        }
+    }
+}
---
+++ PetVet/Models/Mascota.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PetVet.Models
+{
+    public class Mascota
+    {
+        public string Nombre { get; set; }
+        public string Raza { get; set; }
+        public string Edad { get; set; }
+        public string Especie { get; set; }
+        public string Sexo { get; set; }
+        public string Color { get; set; }
+        public bool Esterilizado { get; set; }
+        public string Veterinaria { get; set; }
+    }
+}
